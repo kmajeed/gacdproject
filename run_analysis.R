c00f524796853bc63c9ef75b77c61b48a191bdbd
Khurram Majeed <taurian15581@yahoo.com>
2014-08-21 19:40:37 +0100

    Initial commit
    Just added project files

---
+++ run_analysis.R
@@ -0,0 +1,16 @@
+# ________________________________________________________________________
+# Project for Getting and cleaning data
+# Author : Khurram Majeed
+# Course Iteration : August 2014
+# ________________________________________________________________________
+# Problem Description : The aim of this script file is to perform following:
+#
+# 1. Merge the training and the test sets to create one data set.
+# 2. Extract only the measurements on the mean and standard deviation 
+#    for each measurement.
+# 3. Use descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive activity names.
+# 5. Create a second, independent tidy data set with the average of each 
+#    variable for each activity and each subject.
+# ________________________________________________________________________
+
